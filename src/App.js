8288105f90b182f0d1ea8e004db2f6676d8691bd
Kundan Gahalot <gahalotkundan@gmail.com>
2025-08-07 10:41:14 +0530

    Added Friend and FriendList components

--- src/App.js
+++ src/App.js
@@ -18,3 +18,48 @@ const initialFriends = [
     balance: 0,
   },
 ];
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendSList />
+      </div>
+    </div>
+  );
+}
+
+function FriendSList() {
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend key={friend.id} friend={friend} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <>
+      <li>
+        <img src={friend.image} alt={friend.name} />
+        <h3>{friend.name}</h3>
+
+        {friend.balance < 0 && (
+          <p className="red">
+            You owe {friend.name} {friend.balance}$
+          </p>
+        )}
+        {friend.balance > 0 && (
+          <p className="green">
+            {friend.name} owes you {friend.balance}$
+          </p>
+        )}
+        {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+        <button className="button">Select</button>
+      </li>
+    </>
+  );
+}
